9d06f43d69c01d45a1293bb09d9ec83ba8ed5754
Monica <mroldanaranda@gmail.com>
2023-04-18 19:18:13 +0200

    Versión con soporte para scanner via ficheros

--- scanner.py
+++ scanner.py
@@ -1,11 +1,29 @@
 import socket # Librería puerto
 
-host = input("Introduce la IP a escanear: ")
-ports = input("Introduce los puertos a escanear (separados con coma): ")
+def leer_archivo(filename): # No tenemos por qué definir el tipo de objeto que estamos pasando, pero es recomendable.
+    # Con el archivo abierto haz lo siguiente:
+    with open(filename, "r") as file: #r de read
+        lineas = file.readlines()
+        lineaslimpias=[]
+        for linea in lineas:
+            lineaslimpias.append(linea.rstrip())
+        return lineaslimpias
 
-# Esto va a separar los puertos que hemos leído por consola
-ports = ports.split(",")
+hosts = leer_archivo('ips.txt')
+print(hosts)
+ports = leer_archivo('puertos_comunes.txt')
 
-# Nos dice la cantidad de puertos guardados en la array ports
-print(len(ports))
+# Para cada host que hay en hosts
+for host in hosts:
+    # Para cada puerto que haya en puertos
+    for port in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((host, int(port)))
+
+        if result == 0:
+            print (f"El puerto {port} está abierto en el host {host}")
+        else:
+            print (f"El puerto {port} está cerrado en el host {host}")
+        sock.close()
 
